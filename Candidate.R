7ed792ff506229d365bfd56ef32b7adc714d80c4
davidflast <davidflasterstein@wustl.edu>
2016-03-21 00:00:01 -0500

    Create skeleton of package and create s4 class

---
+++ Candidate.R
@@ -0,0 +1,35 @@
+#' A squared value object 
+#' 
+#' Object of class \code{Candidate} as created by the \code{Candidate} functions
+#'
+#' 
+#' An object of the class `Candidate' has the following slots:
+#' \itemize{
+#' \item \code{name} The added or subtracted squared values
+#' \item \code{delegatesWon} The added or subtracted squared values
+#' \item \code{party} The first input
+#' \item \code{delegatesNeeded} the second input 
+#' }
+#'
+#' @author David P. Flasterstein: \email{davidflasterstein@@wustl.edu}
+#' @aliases AllSquares-class initialize,AllSquares-method getSquares,AllSquares-method 
+#' @rdname Candidate
+#' @export
+setClass(Class="Candidate",
+         representation = representation(
+           name = "string",
+           delegatesWon = "numeric",
+           party = "string"
+           delegatesNeeded = "numeric"
+         ),
+         prototype = prototype(
+           name = string(),
+          delegatesWon = numeric(),
+           party = string(),
+           delegatesNeeded = numeric()
+         )
+)
+
+
+
+
---
+++ Set up code.R
@@ -0,0 +1,13 @@
+
+# Load in necessary libraries and set working directory
+library(devtools)
+library(roxygen2)
+setwd("/Users/davidflast/Documents/PS6")
+
+# Creates a skeleton of a package to put files into
+package.skeleton(name="primaryRace")
+
+# Continously update to create man files
+current.code <- as.package("primaryRace")
+load_all(current.code)
+document(current.code)
