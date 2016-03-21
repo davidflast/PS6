e69cff8e780ccb9089165778c475b549024ff1bf
davidflast <davidflasterstein@wustl.edu>
2016-03-21 00:37:10 -0500

    Added the ability to create a candidate

---
+++ primaryRace/R/createCandidate.R
@@ -0,0 +1,28 @@
+#' Create objects of class candidate
+#'
+#' Creates an object of class candidate using some inputs
+#'
+#' @param x A numeric object
+#' @param y A numeric object with the same dimensionality as \code{x}.
+#'
+#' @return An object of class Candidate containing
+#'  \item{name}{the name of the candidate}
+#'  \item{delegatesWon}{The number of delegates won} 
+#'  \item{party}{The political party of the candidate}
+#' @author David Flaserstein
+#' @examples
+#' 
+#' createCandidate(name = "Hillary", delegatesWon = 100, party = "democrat")
+#' @seealso \code{\link{Candidate}}
+#' @rdname createCandidate
+#' @export
+#' @export
+createCandidate <-function(name, delegatesWon, party){
+    if (party == "Democrats") delegatesNeeded = 2383 - delegatesWon
+    if (party == "Republicans") delegatesNeeded =  1237 - delegatesWon
+    candidate <- new("Candidate", name = name, 
+                      delegatesWon = delegatesWon,
+                      party = party,
+                      delegatesNeeded = delegatesNeeded)
+               return(candidate)
+} 
